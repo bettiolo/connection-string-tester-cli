ba483c1100afb8a3f7c46cc1593756eaed8887f2
Marco Bettiolo <marco.bettiolo@7digital.com>
2014-03-25 11:50:05 +0000

    Initial commit

---
+++ ConnectionStringTester/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace ConnectionStringTester
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			SqlConnection conn = null;
+			Console.WriteLine("Insert connection string:");
+			var connectionString = Console.ReadLine();
+			try
+			{
+				conn = new SqlConnection(connectionString);
+				conn.Open();
+				Console.WriteLine("Works!");
+			}
+			catch (SqlException sqlE)
+			{
+				Console.WriteLine("Error:");
+				Console.Write(sqlE.Message);
+				Console.WriteLine("---");
+			}
+			finally
+			{
+				if (conn != null) conn.Dispose();
+			}
+			Console.WriteLine("Press any key to exit.");
+			Console.ReadLine();
+		}
+	}
+}
